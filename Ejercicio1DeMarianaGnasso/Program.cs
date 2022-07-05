cbaae9c3d9e9896719d03bd6a92e0348545d7737
Usuario <Usuario@GABINETE-09>
2022-07-05 20:00:56 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio1DeMarianaGnasso/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1DeMarianaGnasso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int velocidad, tiempo, distancia, tiempoEnSeg;
+            double distanciaEnKM;
+
+            Console.WriteLine("Ingresar la velocidad en m/s:");
+            velocidad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresar el tiempo en minutos;");
+            tiempo = Convert.ToInt32(Console.ReadLine());
+
+            tiempoEnSeg = tiempo * 60;
+            distancia = velocidad * tiempoEnSeg;
+            distanciaEnKM = distancia * 0.001;
+
+            Console.WriteLine($" La distancia recorrida es de {distanciaEnKM} kilometros")
+;
+            Console.ReadLine();
+        }
+    }
+}
